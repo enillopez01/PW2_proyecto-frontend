463ff356d518913590b3a710b543d449e8b821e1
enillopez01 <62492789+enillopez01@users.noreply.github.com>
2024-04-03 23:20:15 -0600

    Ver0.2.1_frontend
    Ver0.2.1_frontend

---
+++ js/gallerygridselector.js
@@ -0,0 +1,40 @@
+// Get the elements with class="column"
+let elements = document.getElementsByClassName("photocolumn");
+
+// Declare a loop variable
+let i;
+
+// Two images side by side
+function one() {
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.msFlex = "100%";  // IE10
+        elements[i].style.flex = "100%";
+    }
+}
+
+// Two images side by side
+function two() {
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.msFlex = "50%";  // IE10
+        elements[i].style.flex = "50%";
+    }
+}
+
+// Four images side by side
+function four() {
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.msFlex = "25%";  // IE10
+        elements[i].style.flex = "25%";
+    }
+}
+
+// Add active class to the current button (highlight it)
+let header = document.getElementById("galleryheader");
+let btns = header.getElementsByClassName("gallery_btn");
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        let current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
